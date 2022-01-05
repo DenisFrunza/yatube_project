acf9ee38e9d4258ac22e31e8309ac31ac51cdb08
DenisFrunza <frunza-den@yandex-team.ru>
2022-01-05 11:02:31 +0300

    add group posts view and refactor templates

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,20 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from posts.models import Group, Post
+
+POSTS_PER_PAGE = 10
 
 
 def index(request):
+    latest = Post.objects.select_related('group')[:POSTS_PER_PAGE]
+    return render(request, 'posts/index.html', {'posts': latest})
+
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group.objects.prefetch_related('posts'), slug=slug)
+    posts = group.posts.all()[:POSTS_PER_PAGE]
     context = {
-        'title': 'Это главная страница проекта Yatube',
-    }
-    return render(request, 'posts/index.html', context)
-
-
-def group_posts(request):
-    context = {
-        'title': 'Группы',
-        'text': 'Здесь будет информация о группах проекта Yatube',
-
+        'group': group,
+        'posts': posts,
     }
     return render(request, 'posts/group_list.html', context)
--- yatube/settings.py
+++ yatube/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'posts',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -38,6 +39,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'yatube.urls'
@@ -113,3 +116,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+
+]
